b6cb04b9d0abb7cfee665564642317a1d36126ca
saltPillars <30701703+saltPillars@users.noreply.github.com>
2023-05-22 19:36:28 -0500

    Variable Intro

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
-print("Hello Python World!")
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+message = "Hello Python Crash Course reader!"
+print(message)
