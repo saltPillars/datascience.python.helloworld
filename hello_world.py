b633573679695e279fff5c8fb7d0fd97f30c9896
saltPillars <30701703+saltPillars@users.noreply.github.com>
2023-05-22 20:14:41 -0500

    Sytax Errors with Strings.

--- hello_world.py
+++ hello_world.py
@@ -1,33 +1,8 @@
-# Whitespace Fun
+# Syntax Errors with Strings
 
-# \t adds a tab
-print("\tPython")
+# Correct use of single and double quotes
+message = "One of Python's strengths is its diverse community."
+print(message)
 
-# \n adds a new line
-print("Languages:\nPython\nC\nJavaScript")
-
-# can use multiple in one string
-print("Languages:\n\tPython\n\tC\n\tJavascript")
-
-# Whitespace in a string is considered significant unless otherwise told
-# Whitespace can be removed to sanitize data
-
-# .rstrip() removes whitespace from the end of a string
-favorite_language = 'python '
-print(f"{favorite_language}|")
-print(f"{favorite_language.rstrip()}|")
-
-# .rstrip() only removes the whitespace on the fly temporarily
-print(f"{favorite_language}|")
-favorite_language = favorite_language.rstrip()
-print(f"{favorite_language}|")
-
-# .lstrip() removes whitespace from the beginning of a string
-favorite_language = '  python'
-print(f"{favorite_language}")
-print(f"{favorite_language.lstrip()}")
-
-# .strip() removes whitespace from the beginning and end of a stringfavorite_language = '  python'
-favorite_language = '   python    '
-print(f"|{favorite_language}|")
-print(f"|{favorite_language.strip()}|")
+# This throws sytax error
+# message = 'One of Python's strengths is its diverse community.'
