15c70f66807c1864eecad18ad6e0b2fa8a5c4601
saltPillars <30701703+saltPillars@users.noreply.github.com>
2023-05-22 19:52:40 -0500

    Intro to Strings

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,26 @@
-message = "Hello Python World!"
+name = "ada lovelace"
+
+# prints literal string
+print(name)
+
+# prints Ada Lovelace
+print(name.title())
+
+# prints uppercase
+print(name.upper())
+
+# f-strings are used for formatting
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+print (f"Hello, {full_name.title()}!")
+
+# f-strings can be stored in variables
+full_name = f"{first_name} {last_name}"
+message = f"Welcome, {full_name.title()}"
 print(message)
 
-message = "Hello Python Crash Course World!"
-print(message)
-
-message = "Hello Python Crash Course reader!"
-print(message)
+# Use format() if using Python 3.5 or earlier instead of f-strings
+full_name = "{} {}".format(first_name, last_name)
+print(full_name)
